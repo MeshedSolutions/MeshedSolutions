c84bc85936a5c63336b45c49cd3181662fa60eac
Mark Brown <marksbrown@gmail.com>
2015-03-12 22:20:43 -0400

    Create site with .Net MVC

---
+++ MeshedSolutions/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+
+namespace MeshedSolutions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ MeshedSolutions/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeshedSolutions.MvcApplication" Language="C#" %>
